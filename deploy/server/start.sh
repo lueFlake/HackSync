4c67d821c825d2da9159cf6f94ab2e15e9eb0a7c
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2025-05-03 16:09:49 +0300

    Update start.sh

--- deploy/server/start.sh
+++ deploy/server/start.sh
@@ -5,10 +5,10 @@ export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Navigate to server directory
-cd /path/to/HackSync/server/general
+cd /../..server/general
 
 # Build the application
 ./gradlew build
 
 # Start the server
-java -jar build/libs/general-0.0.1.jar 
+java -jar build/libs/hacksync-backend-0.0.1.jar
