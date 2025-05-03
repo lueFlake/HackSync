1a82a9dcef7a217bee725a8c2071a3e16b77734a
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2025-05-03 16:11:05 +0300

    Update start.sh

--- deploy/server/start.sh
+++ deploy/server/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set environment variables
-export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Navigate to server directory
-cd /../..server/general
+cd /../../server/general
 
 # Build the application
 ./gradlew build
